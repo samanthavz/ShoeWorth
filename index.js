8672332ad28fc8f6989931a3300009f7fcdff7ab
samantha van zandwijk <samanthavanzandwijk@gmail.com>
2021-09-28 22:45:31 +0200

    first

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+// Express config
+const app = express();
+const port = 3000;
+
+app.use("/static", express.static(path.join(__dirname, "tm-my-image-model")));
+console.log(__dirname);
+
+app.use("/static", express.static(path.join(__dirname, "scripts")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+//connect
+app.listen(process.env.PORT || port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ scripts/model.js
@@ -0,0 +1,61 @@
+
+// the link to your model provided by Teachable Machine export panel
+const URL = "static/";
+let model, webcam, labelContainer, maxPredictions;
+
+let result = {};
+
+// Load the image model and setup the webcam
+async function init() {
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
+
+  // load the model and metadata
+  // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+  // or files from your local hard drive
+  // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+  model = await tmImage.load(modelURL, metadataURL);
+  maxPredictions = model.getTotalClasses();
+
+  // Convenience function to setup a webcam
+  const flip = true; // whether to flip the webcam
+  webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+  await webcam.setup(); // request access to the webcam
+  await webcam.play();
+  window.requestAnimationFrame(loop);
+
+  // append elements to the DOM
+  document.getElementById("webcam-container").appendChild(webcam.canvas);
+  labelContainer = document.getElementById("label-container");
+  for (let i = 0; i < maxPredictions; i++) {
+    // and class labels
+    labelContainer.appendChild(document.createElement("div"));
+  }
+}
+
+async function loop() {
+  webcam.update(); // update the webcam frame
+  await predict();
+  window.requestAnimationFrame(loop);
+
+    // if (result["Vans"] > 0.8) {
+    //   document.body.style.backgroundColor = "red";
+    // } else if (result["Nike"] > 0.8) {
+    //   document.body.style.backgroundColor = "blue";
+    // } else if (result["Adidas"] > 0.8) {
+    //   document.body.style.backgroundColor = "green";
+    // }
+}
+
+// run the webcam image through the image model
+async function predict() {
+  // predict can take in an image, video or canvas html element
+  const prediction = await model.predict(webcam.canvas);
+
+  result = {};
+  for (let i = 0; i < maxPredictions; i++) {
+    const classPrediction =
+      prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+    labelContainer.childNodes[i].innerHTML = classPrediction;
+  }
+}
